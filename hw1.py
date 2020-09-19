d057fed3d711b93a6dd234391d5df7cbf6a937bf
Boris13373 <boris2010baranov@yandex.ru>
2020-09-19 14:40:03 +0300

    Lesson6

---
+++ hw1.py
@@ -0,0 +1,15 @@
+from time import sleep
+class TrafficLight:
+    def running (self):
+        while True:
+            print("Красный")
+            sleep(7)
+            print("Желтый")
+            sleep(2)
+            print("Зеленый")
+            sleep(7)
+            print("Желтый")
+            sleep(2)
+
+trafficlight = TrafficLight()
+trafficlight.running()
